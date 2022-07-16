e0abc531746c3322d6feae62c51f2408857888e8
Guy Kadosh <guyk50@gmail.com>
2022-07-16 17:25:24 +0300

    simple home design

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,15 @@
 <template>
-  <main class="home full flex justify-between">
+  <main class="home flex justify-between">
     <div class="home__text-box">
-      <h1>Welcome to misterToy</h1>
+      <h1>
+        Welcome to
+        <span class="home__logo">
+          <span>mister</span>
+          <span>T</span>
+          <span>oy</span>
+        </span>
+      </h1>
+      <router-link class="home__btn" to="/toy">Get inside</router-link>
     </div>
     <img class="home__img" src="@/assets/home-page.png" />
   </main>
